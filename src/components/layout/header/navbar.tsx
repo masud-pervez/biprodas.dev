c3b054915f9ea3324f549b1e220356b6390ec8fd
masuddev <tuhin.semicolonit@gamil.com>
2023-10-25 12:14:23 +0600

    portfolio

--- src/components/layout/header/navbar.tsx
+++ src/components/layout/header/navbar.tsx
@@ -1,39 +1,57 @@
-'use client';
-
-import Link from 'next/link';
-import { CurrentUser } from '@/types';
-import useScroll from '@/hooks/use-scroll';
-import { cn } from '@/lib/utils';
-import { ModeToggle } from '@/components/shared/mode-toogle';
+'use client'
+import { useState } from 'react'
+import Link from 'next/link'
+import useScroll from '@/hooks/use-scroll'
+import { cn } from '@/lib/utils'
+import { CurrentUser } from '@/types'
+import { ModeToggle } from '@/components/shared/mode-toogle'
 
 const Navbar = ({ currentUser }: { currentUser: CurrentUser }) => {
-  const scrolled = useScroll(50);
+  const scrolled = useScroll(50)
+  const [active, setActive] = useState('home')
+
+  const navItem = [
+    { key: 'home', title: 'Home' },
+    { key: 'about', title: 'About' },
+    { key: 'works', title: 'Works' },
+    { key: 'team', title: 'My Team' },
+    { key: 'contact', title: 'Contact' },
+  ]
 
   return (
     <header
       className={cn(
-        'sticky top-0 z-30 w-full bg-transparent ',
-        scrolled
-          ? ' h-14 border-b backdrop-blur-2xl transition-all duration-300 ease-in-out'
-          : ' h-20'
+        'fixed top-0 z-40 h-14 w-full',
+        scrolled ? 'shadow backdrop-blur-lg' : 'bg-transparent',
       )}
     >
-      <div className="container mx-auto flex items-center justify-between p-3">
-        <Link href="/" className="text-xl font-bold">
-          Biprodas Roy
+      <div className="container-fluid mx-auto flex items-center justify-between px-5 py-3 lg:px-14">
+        <Link
+          href="/"
+          className="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-xl text-transparent"
+        >
+          <code>Biprodas R.</code>
         </Link>
-        <nav className="space-x-4">
-          <Link href="/home">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/works">Works</Link>
-          <Link href="/teams">My Team</Link>
-          <Link href="/contact">Contact</Link>
+        <nav className="flex items-center justify-start gap-5">
+          {navItem.map(({ key, title }, idx) => (
+            <Link
+              onClick={() => setActive(key)}
+              key={idx}
+              className={`${
+                active === key &&
+                'text-rose-400 transition-all delay-75 duration-75 ease-linear dark:text-cyan-400'
+              } hidden lg:block`}
+              href={`#${key}`}
+            >
+              {title}
+            </Link>
+          ))}
+
           <ModeToggle />
-          <Link href="/contact">Join</Link>
         </nav>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
